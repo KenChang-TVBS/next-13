63ea0c4b85f867225a1b83dc5b999fde28376072
Ken-ChangCK <rockkcor75@gmail.com>
2023-04-07 23:20:43 +0800

    feat:  next api route

--- app/components/Courses.js
+++ app/components/Courses.js
@@ -1,30 +1,31 @@
+"use client";
+
 import Link from "next/link";
 
-async function fetchCoures() {
-  const res = await fetch(`http://localhost:3000/api/courses`);
-  const courses = await res.json();
+// async function fetchCoures() {
+// 	const res = await fetch(`http://localhost:3000/api/courses`);
+// 	const courses = await res.json();
 
-  return courses;
-}
+// 	return courses;
+// }
 
-// const Courses = ({ courses }) => {
-const Courses = async () => {
-  const courses = await fetchCoures();
+const Courses = ({ courses }) => {
+	// const Courses = async () => {
+	// const courses = await fetchCoures();
 
-  console.log("courses", courses);
-  return (
-    <div className="courses">
-      {courses.map((course) => (
-        <div key={course.id} className="card">
-          <h2>{course.title}</h2>
-          <small>Level: {course.level}</small>
-          <p>{course.description}</p>
-          <Link href={course.link} target="_blank" className="btn">
-            Go To Course
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
+	return (
+		<div className="courses">
+			{courses.map(course => (
+				<div key={course.id} className="card">
+					<h2>{course.title}</h2>
+					<small>Level: {course.level}</small>
+					<p>{course.description}</p>
+					<Link href={course.link} target="_blank" className="btn">
+						Go To Course
+					</Link>
+				</div>
+			))}
+		</div>
+	);
 };
 export default Courses;
