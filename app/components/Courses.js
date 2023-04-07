4c8ba4839b240a96695ed85e8b147edbbafddb20
ken <kenchang@tvbs.com.tw>
2023-04-07 18:43:57 +0800

    feat: new nextjs 13 repo

---
+++ app/components/Courses.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+async function fetchCoures() {
+  const res = await fetch(`http://localhost:3000/api/courses`);
+  const courses = await res.json();
+
+  return courses;
+}
+
+// const Courses = ({ courses }) => {
+const Courses = async () => {
+  const courses = await fetchCoures();
+
+  console.log("courses", courses);
+  return (
+    <div className="courses">
+      {courses.map((course) => (
+        <div key={course.id} className="card">
+          <h2>{course.title}</h2>
+          <small>Level: {course.level}</small>
+          <p>{course.description}</p>
+          <Link href={course.link} target="_blank" className="btn">
+            Go To Course
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Courses;
